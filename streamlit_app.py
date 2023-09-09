8383b490fd3f8ca000433c4663c32b0c90fcc2fd
Sherine-6600 <143952151+Sherine-6600@users.noreply.github.com>
2023-09-09 20:30:18 +0530

    kiwi

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,8 +17,8 @@ streamlit.dataframe(fruits_to_show)
 #new section to response fruityvice api response
 streamlit.header("Fruityvice Fruit Advice!")
 import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response) #just writes data to the screen
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "Kiwi")
+
 # Normalize semi-structured JSON data into a flat table 
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # Display a dataframe as an interactive table
