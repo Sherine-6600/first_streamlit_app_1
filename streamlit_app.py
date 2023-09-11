6c177106f56dcaedebd2ae0ed6972789c522a840
Sherine-6600 <143952151+Sherine-6600@users.noreply.github.com>
2023-09-11 15:58:21 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit
 import pandas
 import requests
 import snowflake.connector
-import urllib.error.URLError
+import urllib.error import URLError
 
 streamlit.title("My Parents New Healthy Dinner")
 streamlit.header('Breakfast favorites')
